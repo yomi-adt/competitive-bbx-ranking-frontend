7ca59dfaee5b042be290870a4b9bfd5b52e32995
yomi-adt <nbarlaab@gmail.com>
2025-12-29 18:40:40 -0600

    Started work on nav bar, global styling

--- bbx-2026-frontend/src/main.jsx
+++ bbx-2026-frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import GlobalNavbar from './components/GlobalNavbar.jsx'
+import Container from 'react-bootstrap/esm/Container.js'
+import {Row, Col} from 'react-bootstrap'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Container fluid>
+        <Row>
+            <GlobalNavbar />
+        </Row>
+        <Row className='p-2'>
+            <App />
+        </Row>
+    </Container>
   </StrictMode>,
 )
